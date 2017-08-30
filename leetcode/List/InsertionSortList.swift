f7f37e4e3593e2c320a49a1cc16140ca2db0409d
FFIB <zheng.ffib@gmail.com>
2017-08-30 11:16:27 +0800

    147. Insertion Sort List

---
+++ leetcode/List/InsertionSortList.swift
@@ -0,0 +1,27 @@
+//
+//  InsertionSortList.swift
+//  leetcode
+//
+//  Created by FFIB on 2017/8/30.
+//  Copyright © 2017年 FFIB. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+    func insertionSortList(_ head: ListNode?) -> ListNode? {
+        var origin = head
+        let res = ListNode(0)
+        while let originList = origin {
+            var final = res
+            while let finalList = final.next, finalList.val <= originList.val {
+                final = finalList
+            }
+            let tmp = ListNode(originList.val)
+            tmp.next = final.next
+            final.next = tmp
+            origin = originList.next
+        }
+        return res.next
+    }
+}
