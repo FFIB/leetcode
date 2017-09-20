76e8713a28b216eebd5ef35cf01ab4ddef08db2b
FFIB <604240831@qq.com>
2017-09-20 22:17:22 +0800

    680. Valid Palindrome II

---
+++ leetcode/String/ValidPalindromeII.swift
@@ -0,0 +1,40 @@
+//
+//  ValidPalindromeII.swift
+//  leetcode
+//
+//  Created by FFIB on 2017/9/20.
+//  Copyright © 2017年 FFIB. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+    func validPalindrome(_ s: String) -> Bool {
+        func isPalindrome(chars: ArraySlice<UInt32>) -> Bool {
+            var left = chars.startIndex
+            var right = chars.endIndex - 1
+            while left < right {
+                if chars[left] != chars[right] {
+                    return false
+                }
+                left += 1
+                right -= 1
+            }
+            return true
+        }
+        var chars = Array(s.unicodeScalars.map{$0.value})
+        print(chars)
+        var left = 0
+        var right = s.characters.count - 1
+        while left < right {
+            if chars[left] != chars[right] {
+                print(left)
+                print(right)
+                return isPalindrome(chars: chars[left+1...right]) || isPalindrome(chars: chars[left...right - 1])
+            }
+            left += 1
+            right -= 1
+        }
+        return true
+    }
+}
