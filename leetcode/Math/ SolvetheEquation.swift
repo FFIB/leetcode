4fae92253540e8aec45db4f66c5628507750c7ad
FFIB <604240831@qq.com>
2017-09-26 00:46:15 +0800

    640. Solve the Equation

---
+++ leetcode/Math/ SolvetheEquation.swift
@@ -0,0 +1,57 @@
+//
+//   SolvetheEquation.swift
+//  leetcode
+//
+//  Created by FFIB on 2017/9/25.
+//  Copyright © 2017年 FFIB. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+    func solveEquation(_ equation: String) -> String {
+        func solve(eq: String) -> (Int, Int){
+            var n = 0
+            var x = 0
+            var sign = 1
+            var num = ""
+            let equationArr = Array((eq+"#").characters.map{String($0)})
+            
+            for element in equationArr {
+                let e = Int(element) ?? -1
+                if e >= 0 && e <= 9 {
+                    num += element
+                }else if element == "x" {
+                    x += (Int(num) ?? 1) * sign
+                    num = ""
+                    sign = 1
+                }else {
+                    n += (Int(num) ?? 0) * sign
+                    num = ""
+                    sign = element == "-" ? -1 : 1
+                }
+            }
+            return (x, n)
+        }
+        let equations = equation.components(separatedBy: "=")
+        let lhs = equations.first ?? ""
+        let rhs = equations.last ?? ""
+        let (leftX, leftNum) = solve(eq: lhs)
+        let (rightX, rightNum) = solve(eq: rhs)
+        
+        let x = leftX - rightX
+        let n = rightNum - leftNum
+        print(x)
+        print(n)
+        print(leftNum)
+        print(rightNum)
+        print(leftX)
+        print(rightX)
+        if x != 0 {
+            return "x=\(n / x)"
+        }else if n > 0 {
+            return "No solution"
+        }
+        return "Infinite solutions"
+    }
+}
