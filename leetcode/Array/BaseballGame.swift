8fff8076ae5157f9c4cba822a5d7fc0539662981
FFIB <zheng.ffib@gmail.com>
2017-10-18 10:41:52 +0800

    682. Baseball Game

---
+++ leetcode/Array/BaseballGame.swift
@@ -0,0 +1,33 @@
+//
+//  BaseballGame.swift
+//  leetcode
+//
+//  Created by FFIB on 2017/10/18.
+//  Copyright © 2017年 FFIB. All rights reserved.
+//
+
+import Foundation
+
+extension Solution {
+    func calPoints(_ ops: [String]) -> Int {
+        var last = [Int]()
+        var ans = 0
+        for op in ops {
+            let val: Int
+            switch op {
+            case "C":
+                ans -= last.removeLast()
+                val = 0
+            case "+":
+                val = last.last! + last[last.count - 2]
+            case "D":
+                val = last.last! * 2
+            default:
+                val = Int(num)!
+            }
+            ans += val
+            last.append(val)
+        }
+        return ans
+    }
+}
