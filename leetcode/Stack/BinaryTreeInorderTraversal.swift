b0f77ea37d1d8c181b43d9ee63ebd93f32f494e3
FFIB <zheng.ffib@gmail.com>
2017-12-27 11:35:29 +0800

    94. Binary Tree Inorder Traversal

---
+++ leetcode/Stack/BinaryTreeInorderTraversal.swift
@@ -0,0 +1,42 @@
+//
+//  BinaryTreeInorderTraversal.swift
+//  leetcode
+//
+//  Created by FFIB on 2017/12/27.
+//  Copyright © 2017年 FFIB. All rights reserved.
+//
+
+import Foundation
+
+//94. Binary Tree Inorder Traversal
+/*
+ Given a binary tree, return the inorder traversal of its nodes' values.
+ 
+ For example:
+ Given binary tree [1,null,2,3],
+ 1
+ \
+ 2
+ /
+ 3
+ return [1,3,2].
+ 
+ Note: Recursive solution is trivial, could you do it iteratively?
+ */
+extension  Solution {
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        var stack = [root]
+        var ans = [Int]()
+        var tree = root
+        while tree != nil || !stack.isEmpty {
+            while let left = tree?.left {
+                stack.append(left)
+                tree = left
+            }
+            tree = stack.removeLast()
+            ans.append(tree!.val)
+            tree = tree?.right
+        }
+        return ans
+    }
+}
