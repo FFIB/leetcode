7e0e842d89053a95a400a340d7a0cbceedd28f2f
FFIB <604240831@qq.com>
2018-04-23 11:29:58 +0800

    804. Unique Morse Code Words

---
+++ leetcode/String/UniqueMorseCodeWords.swift
@@ -0,0 +1,58 @@
+//
+//  UniqueMorseCodeWords.swift
+//  leetcode
+//
+//  Created by FFIB on 2018/4/23.
+//  Copyright © 2018 FFIB. All rights reserved.
+//
+
+import Foundation
+
+//804. Unique Morse Code Words
+/*
+ International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b" maps to "-...", "c" maps to "-.-.", and so on.
+
+ For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+ [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ Now, given a list of words, each word can be written as a concatenation of the Morse code of each letter. For example, "cab" can be written as "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). We'll call such a concatenation, the transformation of a word.
+
+ Return the number of different transformations among all words we have.
+
+ Example:
+ Input: words = ["gin", "zen", "gig", "msg"]
+ Output: 2
+ Explanation:
+ The transformation of each word is:
+ "gin" -> "--...-."
+ "zen" -> "--...-."
+ "gig" -> "--...--."
+ "msg" -> "--...--."
+
+ There are 2 different transformations, "--...-." and "--...--.".
+
+
+ Note:
+
+ The length of words will be at most 100.
+ Each words[i] will have length in range [1, 12].
+ words[i] will only consist of lowercase letters.
+ */
+extension Solution {
+    func uniqueMorseRepresentations(_ words: [String]) -> Int {
+        var morseCode = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+                         "-.-",".-..","--","-.","---",".--.","--.-",".-.","...",
+                         "-","..-","...-",".--","-..-","-.--","--.."]
+        var unique = Set<String>()
+
+        for word in words {
+            var str = ""
+            for char in word.unicodeScalars {
+                
+                str.append(morseCode[Int(char.value - 96)])
+            }
+            unique.insert(str)
+        }
+        return unique.count
+    }
+}
